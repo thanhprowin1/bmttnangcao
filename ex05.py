a091cd0c8a07eb1427a905f09ab65ab9676c1d54
Hữu Khang <185398493+thanhprowin1@users.noreply.github.com>
2025-03-12 13:28:20 +0700

    Delete ex05.py

--- ex05.py
+++
@@ -1,6 +0,0 @@
-so_gio_lam= float(input("Nhập số giờ làm: "))
-luong_gio = float(input("Nhập lương mỗi giờ: "))
-gio_tieu_chuan = 44
-gio_vuot_chuan = max(0,so_gio_lam - gio_tieu_chuan)
-thuc_linh = so_gio_lam * luong_gio + gio_vuot_chuan * luong_gio * 1.5
-print(f"Thực lĩnh của bạn là:  {thuc_linh}")
